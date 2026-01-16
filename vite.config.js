eeeb91d0edad7a39907d430ba4bb7d74abecc8b4
ruthiguta <cirurue@gmail.com>
2026-01-16 10:50:43 +0300

    done

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === "production" ? "/elan-spa/" : "/",
+  base:'/elan-spa/', 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
